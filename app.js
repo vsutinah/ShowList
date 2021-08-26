efe20c94bc4d04789359c7de4527732cd7f02c5b
vsutinah <52406232+vsutinah@users.noreply.github.com>
2021-08-26 15:23:14 +1000

    Setup build script for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require('express'),
 	axios = require('axios'),
 	cors = require('cors'),
 	connectDB = require('./config/db');
+path = require('path');
 
 connectDB();
 
@@ -26,7 +27,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/shows', require('./routes/api/shows'));
 
-app.get('/', (req, res) => res.send('API running!'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
